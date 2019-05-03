7d8f84946009f6b3a41d83776b0ca54d1663e1b5
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-03 19:12:30 -0400

    Add mouseover/mouseleave for logo

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// mouseover, mouseleave for Fun Bus logo
+const logoHeadHover = document.querySelector('.nav-container .logo-heading');
+logoHeadHover.addEventListener('mouseover', function (e) {
+    e.target.style.fontSize = '5rem';
+    e.target.style.cursor = 'pointer';
+});
+logoHeadHover.addEventListener('mouseleave', function (e) {
+    e.target.style.fontSize = '';
+});
