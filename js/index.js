62ff0e17f65c260f834575121bb25b1a947461ce
Cobrettie <cobrettiegarner3@gmail.com>
2019-05-03 20:50:21 -0400

    Add dblclick, Add mouseover,mouseleave

--- js/index.js
+++ js/index.js
@@ -5,7 +5,29 @@ const logoHeadHover = document.querySelector('.nav-container .logo-heading');
 logoHeadHover.addEventListener('mouseover', function (e) {
     e.target.style.fontSize = '5rem';
     e.target.style.cursor = 'pointer';
+    e.target.style.transitionDuration = '.2s';
+
 });
 logoHeadHover.addEventListener('mouseleave', function (e) {
     e.target.style.fontSize = '';
+});
+
+
+// mouseover, mouseleave for nav-items
+const navItemHover = document.querySelector('.nav');
+navItemHover.addEventListener('mouseover', function (e) {
+    e.target.style.color = 'dodgerblue';
+    e.target.style.fontSize = '2rem';
+    e.target.style.transitionDuration = '.2s';
+});
+navItemHover.addEventListener('mouseout', function (e) {
+    e.target.style.color = 'black';
+    e.target.style.fontSize = '1.6rem';
+});
+
+
+// scale for fun-bus.jpg
+const scaleImg = document.querySelector('img');
+scaleImg.addEventListener('dblclick', function () {
+    scaleImg.classList.toggle('large');
 });
